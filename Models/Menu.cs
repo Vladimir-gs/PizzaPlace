a0b4d4df851ea03262b30b176aa15c61a6a5d77f
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-20 16:22:16 -0400

    Agregamos la clase menu

---
+++ Models/Menu.cs
@@ -0,0 +1,17 @@
+
+using Pizzeria.Model;
+
+namespace PizzaPlace.Menu
+{
+    public class Menu
+    {
+        public List<Pizza> Pizzas {get; set;} 
+            = new List<Pizza>();
+
+        public void Add(Pizza pizza)
+            => Pizzas.Add(pizza);
+
+        public Pizza? GetPizza(int id)
+            => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
