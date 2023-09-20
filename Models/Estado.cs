851a482df467cae1aa129403ad7bf69e4fcf0a98
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-20 17:47:17 -0400

    Agregamos imports

--- Models/Estado.cs
+++ Models/Estado.cs
@@ -5,6 +5,6 @@ namespace PizzaPlace.Models
         public Menu Menu { get;} = new Menu();
         public CanastaCompras Canasta {get;} = new CanastaCompras();
         public UI UI {get; set;} = new UI();
-
+        public decimal PrecioTotal => Canasta.Pedidos.Sum(id => Menu.GetPizza(id)!.Precio);
     }
 }
