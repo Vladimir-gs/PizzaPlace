b38671205cef5e41275143a6e9db9319d438f1b5
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-09-20 16:16:56 -0400

    Agregamo clase Pizza

---
+++ Models/Pizza.cs
@@ -0,0 +1,26 @@
+namespace Pizzeria.Model
+{
+    public enum Especias
+    {
+        Ninguna,
+        Condimentado,
+        Picante
+    }
+
+    public class Pizza
+    {
+        public Pizza(int id, string nombre, decimal price,
+        Especias especias)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = price;
+            this.Especias = especias;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Especias Especias { get; }
+    }
+}
+
